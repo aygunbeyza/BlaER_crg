822287cb6e231743cf134e33f394328f2c355526
Beyza Aygun <baygun@login1.hpc.crg.es>
2025-06-23 18:58:43 +0200

    j

--- long_read/nextflow/work/ed/d382603aeb57656ae70b05c772b47a/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p result_nanoplot/FAX00328_20240115_1815_946aeace
-NanoPlot --ubam FAX00328_20240115_1815_946aeace.bam -o result_nanoplot/FAX00328_20240115_1815_946aeace --threads 4 --NS0
--- long_read/nextflow/work/fd/f3d9adc1228da975df5493d2946726/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p result_nanoplot/FAX00264_20240115_1817_5ed29589
-NanoPlot --ubam FAX00264_20240115_1817_5ed29589.bam -o result_nanoplot/FAX00264_20240115_1817_5ed29589 --threads 4 --NS0
