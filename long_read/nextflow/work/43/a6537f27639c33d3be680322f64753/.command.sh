44de015268f8cff7a89ddc2090939ebcb26d3632
Beyza Aygun <baygun@login1.hpc.crg.es>
2025-06-24 19:34:44 +0200

    fixed nanoplot for nextflow

--- long_read/nextflow/work/43/a6537f27639c33d3be680322f64753/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p "result_nanoplot/FAX00328_20240115_1815_946aeace"
-NanoPlot --ubam "FAX00328_20240115_1815_946aeace.bam" -o result_nanoplot/FAX00328_20240115_1815_946aeace --N50 --threads 4
