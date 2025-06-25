17039e1d0d3226bbdb28f985042c0f2bebdd43e5
Beyza Aygun <baygun@login1.hpc.crg.es>
2025-06-25 11:11:04 +0200

    Remove work and .nextflow from tracking

--- long_read/nextflow/work/02/a343e07565ad29b77740640534c2ac/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p "FAX02360_20240115_1630_e5122b92"
-NanoPlot --ubam "FAX02360_20240115_1630_e5122b92.bam" --N50 --threads 4
--- long_read/nextflow/work/4f/725d2993576d3175fecafb93d35164/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p "FAX02366_20240115_1632_794829ec"
-NanoPlot --ubam "FAX02366_20240115_1632_794829ec.bam" --N50 --threads 4
