7a0708f30653f607c81ed58f7baabe411edfa134
aygunbeyza <103945723+aygunbeyza@users.noreply.github.com>
2025-08-28 19:58:30 +0300

    Update gff_compare.sh
    for path

--- long_read/gff_compare/gffcompare/gff_compare.sh
+++ long_read/gff_compare/gffcompare/gff_compare.sh
@@ -7,14 +7,17 @@
 #SBATCH --job-name=gff_compare
 #SBATCH --mail-user=beyza.aygun@crg.eu
 #SBATCH --mail-type=FAIL
+
+
+#1.lyric vs isoq
+#2. ref vs lyric
+#3. ref vs isoq
+
 export PATH=/users/rg/baygun/gff_compare/gffcompare:$PATH
-
-zcat /users/rg/baygun/BlaER_crg/files/long_read_files/lyric_gtf/ont-Crg-sc_Hv3_0+_BLaER101Rep1.HiSS.tmerge.min2reads.splicing_status-all.endSupport-all.gff.gz > lyric.gtf
-
-#gffcompare -r /users/rg/baygun/BlaER_crg/long_read/sqanti/ref/gencode.v47.primary_assembly.annotation.gtf -o t120_gff_compare lyric.gtf /users/rg/baygun/BlaER_crg/long_read/IsoQuant/result_isoquant_t120/OUT/OUT.transcript_models.gtf
 
 gffcompare -r lyric.gtf -o lyric_vs_isoquant isoquant_transcript_models.gtf
 
-#gffcompare -r /users/rg/baygun/BlaER_crg/long_read/sqanti/ref/gencode.v47.primary_assembly.annotation.gtf -o ref_lyric_gff_compare /users/rg/baygun/BlaER_crg/long_read/gff_compare/gffcompare/lyric.gtf
+gffcompare -r *.annotation.gtf -o ref_lyric_gff_compare lyric.gtf
 
-#gffcompare -r /users/rg/baygun/BlaER_crg/long_read/sqanti/ref/gencode.v47.primary_assembly.annotation.gtf -o ref_isoquant_gff_compare isoquant_transcript_models.gtf
+gffcompare -r *.annotation.gtf -o ref_isoquant_gff_compare isoquant_transcript_models.gtf
+
