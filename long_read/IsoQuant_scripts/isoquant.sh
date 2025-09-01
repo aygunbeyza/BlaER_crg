3d770ec4a3498de8149a3bf50ab19943294a1bf3
aygunbeyza <103945723+aygunbeyza@users.noreply.github.com>
2025-09-01 13:39:45 +0300

    Update isoquant.sh

--- long_read/IsoQuant_scripts/isoquant.sh
+++ long_read/IsoQuant_scripts/isoquant.sh
@@ -12,11 +12,11 @@ conda init
 source ~/.bashrc
 conda activate isoquant
 
-isoquant.py --reference /users/rg/baygun/BlaER_crg/ref/anotation_and_hg/hg38.sorted.fa.gz \
---genedb /users/rg/baygun/BlaER_crg/ref/anotation_and_hg/gencode.v47.primary_assembly.annotation.gtf.gz \
+isoquant.py --reference /hg38.sorted.fa.gz \
+--genedb ssembly.annotation.gtf.gz \
 --complete_genedb \
---bam /users/rg/baygun/BlaER_crg/files/long_read_files/align_bam_files_lyric/ont-Crg-sc_HpreCap_0+_BLaER101Rep1.bam \
---data_type nanopore -o /users/rg/baygun/BlaER_crg/long_read/IsoQuant/result_isoquant_t0 \
+--bam lyric.bam \
+--data_type nanopore -o /result_isoquant_t0 \
 --thread 8
 
 
